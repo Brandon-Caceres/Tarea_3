6f9b29fbbaf1d2928cc94bfd7571553a0574f781
Your Name <you@example.com>
2025-05-22 18:24:39 -0400

    Actualizacion Brandon

--- main.c
+++ main.c
@@ -30,8 +30,23 @@ typedef struct{
     char esFinal[10];
 }Escenarios;
 
+typedef struct{
+    char nombre[50];
+    Escenarios *actual;
+    List *inventario;
+    int peso;
+    int puntaje;
+    float tRestante;
+}Jugador;
+
 void mostrarMenuPrincipal() {
     limpiarPantalla();
+    //encontre el meme jajaja
+    /*albion online es un mmorpg no lineal en el que escribes 
+    tu propia historia sin limitarte a seguir un camino prefijado, 
+    explora un amplio mundo abierto con cinco biomas unicos, todo 
+    cuanto hagas tendra su repercusíon en el mundo, con su economia 
+    orientada al jugador de albion los jugadores crean...*/
     puts("========================================");
     puts("            MMORPG NO LINEAL          ");
     puts("========================================");
@@ -42,17 +57,14 @@ void mostrarMenuPrincipal() {
     puts("4) Salir");
 }
 
-void mostrarMenuJuego() {
+void mostrarMenuSolo() {
     limpiarPantalla();
-    puts("========================================");
-    puts("            MMORPG NO LINEAL          ");
-    puts("========================================");
   
-    puts("1) Lista de items disponibles");
-    puts("2) Tiempo restante");
-    puts("3) Inventario del jugador");
-    puts("4) Direcciones");
-    puts("5) Volver al menu principal");
+    puts("1) Recoger Item(s)");
+    puts("2) Descartar Item(s)");
+    puts("3) Avanzar en una Dirección");
+    puts("4) Reiniciar Partida");
+    puts("5) Salir del Juego");
 }
 
 void leer_escenarios(HashMap * juego){
@@ -104,6 +116,50 @@ void leer_escenarios(HashMap * juego){
     puts("Escenarios cargados con exito");
 }
 
+void mostrar_escenario(Jugador * player){
+    printf("ID: %s, Nombre: %s\n", player->actual->id, player->actual->nombre);
+    printf("Descripcion: %s\n", player->actual->descripcion);
+    
+    if (player->actual->items_disp != NULL && list_first(player->actual->items_disp) != NULL) {
+        printf("Items Disponibles(nombre,peso,valor): \n");
+        for (Item * item = list_first(player->actual->items_disp); item != NULL; item = list_next(player->actual->items_disp)){
+            printf("  - %s, %dkg, valor %d\n", item->nombre, item->peso, item->valor);
+        }
+    }
+    else printf("No se encuentran items en esta sala\n");
+
+    printf("Tiempo restante: %.2f\n", player->tRestante);
+    
+    puts("Inventario: ");
+    if (player->inventario != NULL && list_first(player->inventario) != NULL){
+        for (Item * objeto = list_first(player->inventario); objeto != NULL; objeto = list_next(player->inventario)){
+            printf("  - %s, %dkg, valor %d\n", objeto->nombre, objeto->peso, objeto->valor);
+        }
+    }
+    else printf("   Vacio\n");
+
+    printf("Peso total: %d", player->peso);
+    printf("Puntaje acumulado: %d", player->puntaje);
+    
+    if(strcmp(player->actual->esFinal, "No") == 0){
+        printf("Direcciones posibles: ");
+        if (player->actual->dir_posibles.arriba != -1){
+            printf("Arriba");
+        }
+        if(player->actual->dir_posibles.abajo != -1){
+            printf("Abajo");
+        }
+        if(player->actual->dir_posibles.izquierda != -1){
+            printf("Izquierda");
+        }
+        if(player->actual->dir_posibles.derecha != -1){
+            printf("Derecha");
+        }
+        printf("\n");
+    }
+    else puts("Felicidades lograste salir del laberinto con un buen botin");
+}
+
 int main(){
     
     char opcion;
@@ -119,6 +175,25 @@ int main(){
             leer_escenarios(juego);
             break;
         case '2':
+            mostrarMenuSolo();
+            char op;
+            printf("Ingrese su opcion: ");
+            switch (op)
+            {
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                break;
+            default:
+                break;
+            }
+            scanf("%s", &op);
             break;
         case '3':
             break;
